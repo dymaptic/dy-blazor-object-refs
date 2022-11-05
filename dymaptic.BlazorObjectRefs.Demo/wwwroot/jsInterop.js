5c07ebc14dea8c7633ebccada610fc6906517b21
Tim Purdum <tim@cedarrivertech.com>
2022-11-05 14:21:05 -0500

    Initial Commit

---
+++ dymaptic.BlazorObjectRefs.Demo/wwwroot/jsInterop.js
@@ -0,0 +1,11 @@
+﻿export function getQuill(containerId, dotNetRef, options) {
+    let quill = new Quill(`#${containerId}`, options);
+    let Delta = Quill.import('delta');
+    let change = new Delta();
+    quill.on('text-change', (delta) => {
+        change = change.compose(delta);
+        dotNetRef.invokeMethodAsync('JsTextChanged', change);
+    })
+    
+    return quill;
+}
